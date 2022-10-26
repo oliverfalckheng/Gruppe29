367730fd8f224123a795e495ae63a99f6ecf6c96
martinPatte <116729949+martinPatte@users.noreply.github.com>
2022-10-26 15:14:21 +0200

    Add files via upload

---
+++ øving5ce.py
@@ -0,0 +1,26 @@
+def gjennomsnitt(total,antall):
+    gjennomsnitt= total/antall
+    return gjennomsnitt
+fil= open("C:/Users/Martin/Documents/UIS-1. år/DAT-120/Øving/liste.txt",'r')
+
+resultat=0
+gammelt_resultat=0
+teller=0
+for linje in fil:
+    teller+=1
+    ny_verdi=float(linje)
+    resultat=ny_verdi+gammelt_resultat
+    gammelt_resultat=resultat
+print(f"gjennomsnittsverdien til målingen var {gjennomsnitt(resultat,teller)}")
+
+def maksverdi():
+    fil= open("C:/Users/Martin/Documents/UIS-1. år/DAT-120/Øving/liste.txt",'r')
+
+    a=0
+    for linje in fil:
+        x=float(linje)
+        if x > a:
+            a=x
+        else: x=a
+    return x
+print((f"maksimalverdien til målingen var {maksverdi()}"))
